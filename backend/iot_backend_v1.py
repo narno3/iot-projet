1f89d89ded34fd81acc171226bca7446c1fc9c55
narno3 <nathan.arnould@edu.esiee.fr>
2024-10-08 16:55:31 +0200

    draft : get trajectory

--- backend/iot_backend_v1.py
+++ backend/iot_backend_v1.py
@@ -35,15 +35,27 @@ for ti, event in zip(t, events):
 # we might want to get the satellite's longitude and latitude to print it on some map :
 # https://rhodesmill.org/skyfield/earth-satellites.html#satellite-longitude-latitude-and-height
 # my API:
+
 class SatPass(BaseModel):
     """describes the pass of one satellite."""
-    t_start: str = None
-    t_end: str = None
-    trajectory:list = []
+    t_start: object = None
+    t_end: object = None
+    trajectory:list[float] = []
 
+
+def get_traj(t1, t2, satellite) -> list[list]:
+    """returns 10 points of a trajectory for 1 satellite, from t1 to t2"""
+    n_points = 10
+    delta = (t2 - t1) / n_points
+    points = []
+    for i in range(n_points):
+        geocentric = satellite.at(t1 + i*delta)
+        lat, lon = wgs84.latlon_of(geocentric)
+        points.append([lat.degrees, lon.degrees])
+    return points
 
 def get_passes(time, satellite) -> list[SatPass]:
-    """returns all passes for this satellite."""
+    """returns all passes between time and (time+24h) for this satellite."""
     passes = []
     t, events = satellite.find_events(user_location, t0, t1, altitude_degrees=10.0)
     current_pass = SatPass()
@@ -53,5 +65,10 @@ def get_passes(time, satellite) -> list[SatPass]:
             current_pass = SatPass(t_start = ti)
         if event == 2:
             current_pass.t_end = ti
+            current_pass.trajectory = get_traj(current_pass.t_start, current_pass.t_end, satellite)
             passes.append(current_pass)
-    ...
+    return passes
+
+# get_traj(t0, t1, sats[0])
+passes = get_passes(t1, sats[0])
+print(passes[0])
